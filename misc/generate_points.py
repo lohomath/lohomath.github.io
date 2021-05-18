58254461ed59ce8f61c1f055e3fd505648b4399c
Georg Loho <loho@abraham>
2021-05-18 11:40:57 +0200

    script for background

---
+++ misc/generate_points.py
@@ -0,0 +1,17 @@
+
+from random import randint, random, uniform
+from math import sin, cos, pi
+
+def randomTriangles(screenX, screenY, triangleSize, colorValues, numberTriangles):
+    s = '<polygon points="{},{} {},{} {},{}"' + ' style="fill:rgba({},{},{},{})" />'.format(*colorValues)
+    lines = []
+    for _ in range(numberTriangles):
+        center = randint(0,screenX), randint(0,screenY)
+        angles = [random()*2*pi for _ in range(3)]
+        radii = [uniform(triangleSize/2,triangleSize) for _ in range(3)]
+        unit_vertices = [(radius*cos(angle), radius*sin(angle)) for radius, angle in zip(radii,angles)]
+        t = str(s.format(*[round(c + vel,3) for v in unit_vertices for vel,c in zip(v,center)]))
+        print(t)
+        lines.append(t)
+
+    print(''.join(lines))
